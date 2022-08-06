302b83d110873a0a947b2359886370ddd37e931d
Shruti <sdnk1993@gmail.com>
2022-08-06 19:50:04 +0530

    Commit on Uc2 branch

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,9 +1,16 @@
 #!bin/bash
 
-flip=$((RANDOM%2))
-if [ $flip -eq 0 ]
-then
-	echo "Head wins"
-else
-	echo "Tail wins"
-fi
+head=0;
+tail=0;
+for (( i=0; i<=10;i++ ))
+do 
+	flip=$((RANDOM%2))
+	if [ $flip -eq 0 ]
+	then
+		((head++))
+	else
+		((tail++))
+	fi
+done
+echo "No of Head : $head"
+echo "No of Tail : $tail"
