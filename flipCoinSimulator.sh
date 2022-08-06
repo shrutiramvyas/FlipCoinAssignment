39f5c8c2b570ed26282cb523e91c89b7023392c6
Shruti <sdnk1993@gmail.com>
2022-08-06 20:31:05 +0530

    Commit on branch UC4

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -2,7 +2,6 @@
 
 #!bin/bash
 flip=$((RANDOM%2));
-echo $flip
 head=0;
 tail=0;
 while [ $head -ne 22 -o $tail -ne 22 ]
@@ -10,20 +9,32 @@ do
     if [[ $flip -eq 0 ]] 
     then
         ((tail++));
-        echo "tail : $tail"
+        tailarr=[$tail];
     else
         ((head++));
-        echo "head : $head"
+        headarr=[$head];
     fi
     if [[ $head -eq 22 ]]
-    then    
+    then
+        echo "Tail= ${tailarr[@]}";
+        echo "Head= ${headarr[@]}";    
         echo "Head won";
+
         break;
     elif [[ $tail -eq 22 ]]
-    then
+    then    
+        echo "Tail= ${tailarr[@]}";
+        echo "Head= ${headarr[@]}";
         echo "Tail won";
         break;
     fi
     flip=$((RANDOM%2));  
 done
+if [[ $head -eq $tail ]]
+    then    
+        echo "Tail= ${tailarr[@]}";
+        echo "Head= ${headarr[@]}";
+        echo "Match Tie";
+        break;
+    fi
 
